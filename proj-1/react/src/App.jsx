97e8d9f4067407a9458f357bc9097cbb9d0e8e5a
anukul rai <anukul.rau@gmail.com>
2025-06-10 20:22:12 +0545

    hellyah

---
+++ proj-1/react/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+
+
+function App() {
+    return (<>
+        <Nav />
+        <Hero />
+    </>);
+}
+export default App;
